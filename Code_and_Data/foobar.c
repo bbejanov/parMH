052b41613658e3607438984e587e1f6d5679b84e
Boyan Bejanov <bbejanov@gmail.com>
2014-02-28 06:36:53 +0000

    makefile and foobar.c as an example of how to use RngStreams
    git-svn-id: file:///home/bejb/work/svnrepo-old/comp5704-2014w/trunk/project-website@12 d8f3afd6-4aa3-4a68-856e-a1eacb5524db

---
+++ Code_and_Data/foobar.c
@@ -0,0 +1,58 @@
+
+#include <RngStream.h>
+#include <stdio.h>
+
+#ifndef DEBUG
+#define DEBUG 0!=0
+#endif
+
+enum { MAX_NUM_GARS = 100 };
+
+RngStream init_stream() 
+{
+    RngStream g;
+    g = RngStream_CreateStream("");
+    RngStream_IncreasedPrecis(g, 1);
+    return g;
+}
+
+
+/* 
+    urand - uses RngStreams to generate random numbers
+            from multiple independent streams
+    which_gar - in - stream number, zero-based
+    num_vals  - in - number of values to generate
+    vals      - out - array of size n containing the generated values
+*/
+void urand(int *which_gar, int *num_vals, double *vals) 
+{
+   static int num_gars = -1; 
+   static RngStream gar[MAX_NUM_GARS];
+
+   int w = which_gar[0];
+   int n = num_vals[0];
+   int i;
+
+   if(DEBUG) printf("w = %d, n = %d, v[0] = %g\n", w, n, v[0]);
+
+   if (w < 0) {
+      printf("Invalid stream index (must be > 0)\n");
+      return;
+   }
+
+   if (w >= MAX_NUM_GARS) {
+      printf("Too many streams (max streams = %d)\n", MAX_NUM_GARS);
+      return;
+   }
+
+   while ( w > num_gars ) {
+      if(DEBUG) printf("Initializing stream %d\n", ++num_gars);
+      gar[num_gars] = init_stream();
+   }
+
+   for(i=0 ; i<n; ++i) 
+      vals[i] = RngStream_RandU01(gar[w]);
+
+   return;
+} 
+
